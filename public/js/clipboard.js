a03c0cdecbe15e87f96c198a56327b9a14a12c5b
Daniel Sanchez <arturo97@live.unc.edu>
2022-02-15 13:49:11 -0500

    work on documentation and new staging site

--- public/js/clipboard.js
+++ public/js/clipboard.js
@@ -3,7 +3,8 @@ $( document ).ready(function() {
 
     const codeBlocks = document.querySelectorAll('.code-block');
     const copyCodeButtons = document.querySelectorAll('.copy-code');
-    
+
+
     copyCodeButtons.forEach((copyCodeButton, index) => {
       const code = codeBlocks[index].innerText;
       const icon = copyCodeButtons[index].children[0]
@@ -27,6 +28,49 @@ $( document ).ready(function() {
                 icon.classList.remove("ti-check")
                 button.style.backgroundColor = null
             }, 1000)
+        }
+
+      });
+    });
+
+
+    const codeSnippets = document.querySelectorAll('.code-snippet');
+    const copyCodeSnippetButtons = document.querySelectorAll('.copy-code-snippet');
+    
+    console.log(codeSnippets)
+
+    copyCodeSnippetButtons.forEach((copyCodeButton, index) => {
+      const code = codeSnippets[index].innerText;
+      const icon = copyCodeSnippetButtons[index].children[0]
+      const button = copyCodeSnippetButtons[index]
+
+
+
+      copyCodeButton.addEventListener('click', () => {
+        window.navigator.clipboard.writeText(code.substring(2));
+
+        console.log(icon.classList)
+        if (icon.classList.contains("ti-clipboard")){
+            
+          icon.classList.remove("ti-clipboard")
+          icon.classList.add("ti-check")
+          
+          button.classList.remove('btn-secondary')
+          button.classList.add('btn-success')
+          
+          //icon.classList.add("ti-check")
+
+          console.log("HERE")
+          console.log(icon.classList)
+
+          setTimeout(()=>{
+              icon.classList.add("ti-clipboard")
+              icon.classList.remove("ti-check")
+              
+              button.classList.add('btn-secondary')
+              button.classList.remove('btn-success')
+              
+          }, 1000)
         }
 
       });
